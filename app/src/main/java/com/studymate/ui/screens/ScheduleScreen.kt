4bafdb0670ffbc373ba27f2093a4fdda966944cc
Ilgar <ilgar.coder@gmail.com>
2026-08-12 01:29:49 +0400

    start

---
+++ app/src/main/java/com/studymate/ui/screens/ScheduleScreen.kt
@@ -0,0 +1,21 @@
+package com.studymate.ui.screens
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.studymate.ui.theme.StudyMateTheme
+
+@Composable
+fun ScheduleScreen() {
+
+}
+
+@Preview
+@Composable
+fun ScheduleScreenPreview() {
+    StudyMateTheme {
+        Surface {
+            ScheduleScreen()
+        }
+    }
+}
