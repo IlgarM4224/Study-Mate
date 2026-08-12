e1796380a57e9477cb72c445b9f6cbcdf2a76c2c
Ilgar <ilgar.coder@gmail.com>
2026-08-12 22:02:29 +0400

    test Schedule

--- app/src/main/java/com/studymate/ui/screens/ScheduleScreen.kt
+++ app/src/main/java/com/studymate/ui/screens/ScheduleScreen.kt
@@ -1,21 +1,62 @@
 package com.studymate.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.studymate.data.Lesson
+import com.studymate.data.TestData
+import com.studymate.ui.screens.components.LessonTopAppBar
+import com.studymate.ui.screens.components.LessonsList
 import com.studymate.ui.theme.StudyMateTheme
 
 @Composable
-fun ScheduleScreen() {
-
+fun ScheduleScreen(
+    modifier: Modifier = Modifier,
+    lessons: List<Lesson> = emptyList()
+) {
+    Scaffold(
+        topBar = {
+            LessonTopAppBar(
+                title = "Schedule",
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        LessonsList(
+            lessons = lessons,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
-fun ScheduleScreenPreview() {
+fun ScheduleScreenLightPreview() {
     StudyMateTheme {
         Surface {
-            ScheduleScreen()
+            ScheduleScreen(
+                modifier = Modifier.fillMaxSize(),
+                lessons = TestData.getLessons()
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ScheduleScreenDarkPreview() {
+    StudyMateTheme(darkTheme = true) {
+        Surface {
+            ScheduleScreen(
+                modifier = Modifier.fillMaxSize(),
+                lessons = TestData.getLessons()
+            )
         }
     }
 }
