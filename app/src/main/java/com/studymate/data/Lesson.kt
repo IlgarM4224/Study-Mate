4bef6a2cc0fb6b3b3f15cf971d9e181842341f8b
Ilgar <ilgar.coder@gmail.com>
2026-08-14 18:05:59 +0400

    test data object for previews

--- app/src/main/java/com/studymate/data/Lesson.kt
+++ app/src/main/java/com/studymate/data/Lesson.kt
@@ -8,54 +8,8 @@ data class Lesson(
     val startTime: String
 )
 
-object TestData {
-    fun getLessons() = listOf(
-        Lesson(
-            name = "Programming basics",
-            teacher = "Hijran",
-            type = "Lecture",
-            location = "409",
-            startTime = "8:30",
-        ),
-
-        Lesson(
-            name = "Web technologies",
-            teacher = "Alla",
-            type = "Seminar",
-            location = "401",
-            startTime = "10:15",
-        ),
-
-        Lesson(
-            name = "Mathematical analysis",
-            teacher = "Abbas",
-            type = "Lecture",
-            location = "202",
-            startTime = "12:00",
-        ),
-
-        Lesson(
-            name = "Analytical geometry",
-            teacher = "Adil",
-            type = "Seminar",
-            location = "311",
-            startTime = "13:50",
-        ),
-
-        Lesson(
-            name = "Physics",
-            teacher = "Murad",
-            type = "Lecture",
-            location = "200",
-            startTime = "15:35",
-        ),
-
-        Lesson(
-            name = "Databases",
-            teacher = "Anton",
-            type = "Seminar",
-            location = "405",
-            startTime = "17:20",
-        )
-    )
-}
+data class Day(
+    val day: String,
+    val date: String,
+    val selected: Boolean = false
+)
---
+++ app/src/main/java/com/studymate/data/TestData.kt
@@ -0,0 +1,64 @@
+package com.studymate.data
+
+object TestData {
+    fun getLessons() = listOf(
+        Lesson(
+            name = "Programming basics",
+            teacher = "Hijran",
+            type = "Lecture",
+            location = "409",
+            startTime = "8:30",
+        ),
+
+        Lesson(
+            name = "Web technologies",
+            teacher = "Alla",
+            type = "Seminar",
+            location = "401",
+            startTime = "10:15",
+        ),
+
+        Lesson(
+            name = "Mathematical analysis",
+            teacher = "Abbas",
+            type = "Lecture",
+            location = "202",
+            startTime = "12:00",
+        ),
+
+        Lesson(
+            name = "Analytical geometry",
+            teacher = "Adil",
+            type = "Seminar",
+            location = "311",
+            startTime = "13:50",
+        ),
+
+        Lesson(
+            name = "Physics",
+            teacher = "Murad",
+            type = "Lecture",
+            location = "200",
+            startTime = "15:35",
+        ),
+
+        Lesson(
+            name = "Databases",
+            teacher = "Anton",
+            type = "Seminar",
+            location = "405",
+            startTime = "17:20",
+        )
+    )
+
+    fun getWeek() = listOf(
+        Day(day = "Mon", date = "13 may", selected = true),
+        Day(day = "Tue", date = "14 may"),
+        Day(day = "Wed", date = "15 may"),
+        Day(day = "Thu", date = "16 may"),
+        Day(day = "Fri", date = "17 may",),
+        Day(day = "Sat", date = "18 may"),
+        Day(day = "Sun", date = "19 may"),
+    )
+}
+
