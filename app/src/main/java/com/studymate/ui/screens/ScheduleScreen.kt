60d19f4743c1fba788017219751cebe53b05035f
Ilgar <ilgar.coder@gmail.com>
2026-08-14 18:10:01 +0400

    new UI element

--- app/src/main/java/com/studymate/ui/screens/ScheduleScreen.kt
+++ app/src/main/java/com/studymate/ui/screens/ScheduleScreen.kt
@@ -1,23 +1,56 @@
 package com.studymate.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.studymate.data.Day
 import com.studymate.data.Lesson
 import com.studymate.data.TestData
 import com.studymate.ui.screens.components.LessonTopAppBar
 import com.studymate.ui.screens.components.LessonsList
+import com.studymate.ui.screens.components.WeekRowAndType
 import com.studymate.ui.theme.StudyMateTheme
+
 
 @Composable
 fun ScheduleScreen(
     modifier: Modifier = Modifier,
-    lessons: List<Lesson> = emptyList()
+    viewModel: ScheduleScreenViewModel = ScheduleScreenViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    ScheduleScreenContend(
+        modifier = modifier,
+        lessons = uiState.lessons,
+        week = uiState.week,
+        weekType = uiState.weekType,
+        onEditClick = { viewModel.onEditeClick() },
+        onCardClick = { viewModel.onCardClick() },
+        onDayClick = { viewModel.onDayClick()},
+        onNextWeekClick = { viewModel.onNextWeekClick() },
+        onCurrentWeekClick = { viewModel.onCurrentWeekClick() },
+    )
+}
+@Composable
+fun ScheduleScreenContend(
+    modifier: Modifier = Modifier,
+    lessons: List<Lesson> = emptyList(),
+    week: List<Day> = emptyList(),
+    weekType: String,
+    onEditClick: () -> Unit,
+    onCardClick: () -> Unit,
+    onDayClick: () -> Unit,
+    onNextWeekClick: () -> Unit,
+    onCurrentWeekClick: () -> Unit
 ) {
     Scaffold(
         topBar = {
@@ -28,21 +61,28 @@ fun ScheduleScreen(
         },
         modifier = modifier
     ) { innerPadding ->
-        LessonsList(
-            lessons = lessons,
-            modifier = Modifier.padding(innerPadding)
-        )
-    }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun ScheduleScreenLightPreview() {
-    StudyMateTheme {
-        Surface {
-            ScheduleScreen(
-                modifier = Modifier.fillMaxSize(),
-                lessons = TestData.getLessons()
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            LessonsList(
+                lessons = lessons,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp),
+                onEditClick = onEditClick,
+                onCardClick = onCardClick,
+                weekRowAndType = {
+                    WeekRowAndType(
+                        week = week,
+                        weekType = weekType,
+                        onDayClick = onDayClick,
+                        onNextWeekClick = onNextWeekClick,
+                        onCurrentWeekClick = onCurrentWeekClick,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             )
         }
     }
@@ -50,12 +90,43 @@ fun ScheduleScreenLightPreview() {
 
 @Preview(showSystemUi = true)
 @Composable
-fun ScheduleScreenDarkPreview() {
+fun ScheduleScreenContendLightPreview() {
+    StudyMateTheme {
+        Surface {
+            ScheduleScreenContend(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(4.dp),
+                lessons = TestData.getLessons(),
+                week = TestData.getWeek(),
+                weekType = "Upper",
+                onEditClick = {},
+                onCardClick = {},
+                onNextWeekClick = {},
+                onDayClick = {},
+                onCurrentWeekClick = {}
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ScheduleScreenContendDarkPreview() {
     StudyMateTheme(darkTheme = true) {
         Surface {
-            ScheduleScreen(
-                modifier = Modifier.fillMaxSize(),
-                lessons = TestData.getLessons()
+            ScheduleScreenContend(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(4.dp),
+                lessons = TestData.getLessons(),
+                week = TestData.getWeek(),
+                weekType = "Lower",
+                onEditClick = {},
+                onCardClick = {},
+                onNextWeekClick = {},
+                onDayClick = {},
+                onCurrentWeekClick = {}
             )
         }
     }
---
+++ app/src/main/java/com/studymate/ui/screens/components/WeekRowAndType.kt
@@ -0,0 +1,370 @@
+package com.studymate.ui.screens.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.studymate.data.Day
+import com.studymate.data.TestData
+import com.studymate.ui.theme.StudyMateTheme
+
+@Composable
+fun WeekRowAndType(
+    modifier: Modifier,
+    week: List<Day>,
+    weekType: String,
+    onDayClick: () -> Unit = {},
+    isCurrentWeek: Boolean = true,
+    onCurrentWeekClick: () -> Unit,
+    onNextWeekClick: () -> Unit
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        WeekType(
+            type = weekType,
+            isCurrentWeek = isCurrentWeek,
+            onCurrentWeekClick = onCurrentWeekClick,
+            onNextWeekClick = onNextWeekClick,
+            modifier = Modifier.fillMaxWidth()
+
+        )
+
+        Spacer(Modifier.height(4.dp))
+
+        WeekRow(
+            week = week,
+            onDayClick = onDayClick,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+@Composable
+private fun WeekType(
+    modifier: Modifier = Modifier,
+    type: String,
+    isCurrentWeek: Boolean = true,
+    onCurrentWeekClick: () -> Unit,
+    onNextWeekClick: () -> Unit
+) {
+    Column( modifier = modifier ) {
+        Row{
+            WeekChoiceButton(
+                label = "Current",
+                onClick = onCurrentWeekClick,
+                isCurrentWeek = isCurrentWeek,
+                modifier = Modifier.weight(1f)
+            )
+
+            WeekChoiceButton(
+                label = "Next",
+                onClick = onNextWeekClick,
+                isCurrentWeek = !isCurrentWeek,
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        Text(
+            text = "$type week",
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+@Composable
+private fun WeekChoiceButton(
+    label: String,
+    onClick: () -> Unit,
+    isCurrentWeek: Boolean,
+    modifier: Modifier
+) {
+    val colorScheme = if (isCurrentWeek) MaterialTheme.colorScheme.secondaryContainer else Color.Transparent
+    Box(modifier = modifier) {
+        TextButton(
+            onClick = onClick,
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = colorScheme,
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = label)
+        }
+    }
+}
+
+@Composable
+private fun WeekRow(
+    modifier: Modifier = Modifier,
+    week: List<Day>,
+    onDayClick: () -> Unit = {}
+) {
+    LazyRow(
+        modifier = modifier
+            .clip(RoundedCornerShape(12.dp))
+            .padding(vertical = 8.dp, horizontal = 4.dp)
+    ) {
+        items(week.size) { day ->
+            DayChip(
+                day = week[day].day,
+                date = week[day].date,
+                selected = week[day].selected,
+                onClick = onDayClick,
+            )
+            Spacer(Modifier.width(4.dp))
+        }
+    }
+}
+
+// Day button
+private data class DayChipColor(
+    val text: Color,
+    val background: Color
+)
+@Composable
+private fun DayChip(
+    modifier: Modifier = Modifier,
+    day: String,
+    date: String,
+    selected: Boolean,
+    onClick: () -> Unit
+) {
+    val colorScheme: DayChipColor = when (selected) {
+        true -> DayChipColor(
+            text = MaterialTheme.colorScheme.onPrimaryContainer,
+            background = MaterialTheme.colorScheme.primaryContainer
+        )
+        false -> DayChipColor(
+            text = MaterialTheme.colorScheme.onSurfaceVariant,
+            background = MaterialTheme.colorScheme.surfaceContainerHigh
+        )
+    }
+
+    Column(
+        modifier = modifier
+            .clip(RoundedCornerShape(12.dp))
+            .background(color = colorScheme.background)
+            .clickable(onClick = onClick)
+            .padding(horizontal = 12.dp, vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = day,
+            style = MaterialTheme.typography.labelMedium,
+            color = colorScheme.text
+        )
+
+        Spacer(Modifier.padding(2.dp))
+
+        Text(
+            text = date,
+            style = MaterialTheme.typography.labelSmall,
+            color = colorScheme.text
+        )
+    }
+}
+
+/*
+ * Week Row + Week Type
+ */
+
+@Preview
+@Composable
+fun WeekRowAndTypePreview() {
+    StudyMateTheme {
+        Surface {
+            WeekRowAndType(
+                week = TestData.getWeek(),
+                weekType = "Upper",
+                onDayClick = {},
+                onNextWeekClick = {},
+                onCurrentWeekClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WeekRowAndTypeDarkPreview() {
+    StudyMateTheme(darkTheme = true) {
+        Surface {
+            WeekRowAndType(
+                week = TestData.getWeek(),
+                weekType = "Upper",
+                onDayClick = {},
+                onNextWeekClick = {},
+                onCurrentWeekClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+        }
+    }
+}
+
+/*
+ * Week Type preview
+ */
+
+@Preview(showSystemUi = false)
+@Composable
+fun WeekTypePreview() {
+    StudyMateTheme {
+        Surface {
+            WeekType(
+                type = "Lower",
+                isCurrentWeek = true,
+                onCurrentWeekClick = {},
+                onNextWeekClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = false)
+@Composable
+fun WeekTypeDarkPreview() {
+    StudyMateTheme(darkTheme = true) {
+        Surface {
+            WeekType(
+                type = "Upper",
+                isCurrentWeek = true,
+                onCurrentWeekClick = {},
+                onNextWeekClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+        }
+    }
+}
+
+/*
+ * Week Row preview
+ */
+
+@Preview(showSystemUi = false)
+@Composable
+fun WeekRowPreview() {
+    StudyMateTheme {
+        Surface {
+            WeekRow(
+                modifier = Modifier.fillMaxWidth(),
+                week = TestData.getWeek()
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = false)
+@Composable
+fun WeekRowDarkPreview() {
+    StudyMateTheme(darkTheme = true) {
+        Surface {
+            WeekRow(
+                modifier = Modifier.fillMaxWidth(),
+                week = TestData.getWeek()
+            )
+        }
+    }
+}
+
+/*
+ * Day Chip preview
+ */
+
+@Preview
+@Composable
+fun SelectedDayChipPreview() {
+    StudyMateTheme {
+        Surface {
+            DayChip(
+                day = "Mn",
+                date = "13 may",
+                selected = true,
+                onClick = {},
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SelectedDayChipDarkPreview() {
+    StudyMateTheme(darkTheme = true) {
+        Surface {
+            DayChip(
+                day = "Mn",
+                date = "13 may",
+                selected = true,
+                onClick = {},
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DayChipPreview() {
+    StudyMateTheme {
+        Surface {
+            DayChip(
+                day = "Mn",
+                date = "13 may",
+                selected = false,
+                onClick = {},
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DayChipDarkPreview() {
+    StudyMateTheme(darkTheme = true) {
+        Surface {
+            DayChip(
+                day = "Mn",
+                date = "13 may",
+                selected = false,
+                onClick = {},
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
